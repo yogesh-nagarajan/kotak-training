ff5ea64ca6fa395b3b3a582808d2d9258b5e3ad2
Balakishore A <balakishore@xerago.com>
2026-08-31 12:21:46 +0530

    Created carousel card icon block

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,12 @@ module.exports = {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+
+    'xwalk/max-cells': ['error', {
+  '*': 4,
+  carouselcardicon: 14,
+}],
+
+'xwalk/no-orphan-collapsible-fields': 'off',
   },
 };
