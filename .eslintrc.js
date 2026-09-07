53ded690c140b01145fd35d49000ba6b2059806e
Balakishore A <balakishore@xerago.com>
2026-09-07 17:46:46 +0530

    Fix bento model and lint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
       '*': 4,
       carouselcardicon: 14,
       'safe-banking-card': 5,
+      bento: 10,
       'bento-item': 8,
     }],
     'xwalk/no-orphan-collapsible-fields': 'off',
